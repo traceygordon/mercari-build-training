1df03f1977ba4030753ec80c2d2646fdb009da43
Tracey Gordon <traceyagordon@gmail.com>
2025-03-27 18:36:05 -0400

    updated images and port

--- typescript/simple-mercari-web/src/api/index.ts
+++ typescript/simple-mercari-web/src/api/index.ts
@@ -1,5 +1,5 @@
 
-const SERVER_URL = import.meta.env.VITE_BACKEND_URL || 'http://127.0.0.1:8000';
+const SERVER_URL = import.meta.env.VITE_BACKEND_URL || 'http://127.0.0.1:9000';
 
 export interface Item {
   id: number;
